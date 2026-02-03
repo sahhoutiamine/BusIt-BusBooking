a2c0f3cf35b55b55c5e15fabc1e7f05485ee29c5
sahhoutiamine <midoriaduko@gmail.com>
2026-02-03 10:18:57 +0100

    add Route model

---
+++ app/Models/Route.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nom', 'description'];
+
+    public function etapes()
+    {
+        return $this->hasMany(Etape::class)->orderBy('ordre');
+    }
+
+    public function programmes()
+    {
+        return $this->hasMany(Programme::class);
+    }
+    
+    public function getDetails()
+    {
+        return $this->nom . ': ' . $this->description;
+    }
+}
