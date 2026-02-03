b305a2eb6abde48b27a6f02a14bc44381b3feb3d
sahhoutiamine <midoriaduko@gmail.com>
2026-02-03 10:20:33 +0100

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('search.index');
 });
 
+Route::get('/search', [SearchController::class, 'index'])->name('search.index');
+Route::get('/search/results', [SearchController::class, 'search'])->name('search.results');
+
 Route::get('/hello', function () {
-    // return view('welcome');
-    echo "hello world";
+    return view('welcome');
 });
