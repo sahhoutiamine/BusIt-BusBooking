bd71e2f73ea306e64cdc6b1547c4a70ebcdbdf2f
sahhoutiamine <midoriaduko@gmail.com>
2026-02-03 10:20:14 +0100

    update ReservationSeeder

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -3,11 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Reservation;
-use App\Models\Seat;
-use App\Models\Trip;
 use App\Models\User;
+use App\Models\Segment;
 use Illuminate\Database\Seeder;
-use Carbon\Carbon;
 
 class ReservationSeeder extends Seeder
 {
@@ -16,49 +14,28 @@ class ReservationSeeder extends Seeder
      */
     public function run(): void
     {
-        // Get some passengers
-        $passenger1 = User::where('email', 'mohammed@example.com')->first();
-        $passenger2 = User::where('email', 'amina@example.com')->first();
+        Reservation::truncate();
 
-        // Get a trip
-        $trip = Trip::first();
+        $client = User::where('role', 'client')->first();
+        
+        $segment = Segment::first();
 
-        if ($trip && $passenger1) {
-            // Book a seat for passenger 1
-            $seat1 = Seat::where('trip_id', $trip->id)->where('name', 'S1')->first();
+        if ($client && $segment) {
+            Reservation::create([
+                'user_id' => $client->id,
+                'segment_id' => $segment->id,
+                'date_reservation' => now(),
+                'statut' => 'Payé',
+                'siege_numero' => 12,
+            ]);
             
-            if ($seat1) {
-                Reservation::create([
-                    'user_id' => $passenger1->id,
-                    'trip_id' => $trip->id,
-                    'seat_id' => $seat1->id,
-                    'date_reservation' => Carbon::now(),
-                    'status' => 'paid',
-                    'siege_numero' => 1,
-                ]);
-
-                // Update seat availability
-                $seat1->update(['is_available' => false]);
-            }
-        }
-
-        if ($trip && $passenger2) {
-            // Book a seat for passenger 2
-            $seat2 = Seat::where('trip_id', $trip->id)->where('name', 'S2')->first();
-            
-            if ($seat2) {
-                Reservation::create([
-                    'user_id' => $passenger2->id,
-                    'trip_id' => $trip->id,
-                    'seat_id' => $seat2->id,
-                    'date_reservation' => Carbon::now(),
-                    'status' => 'pending',
-                    'siege_numero' => 2,
-                ]);
-
-                // Update seat availability
-                $seat2->update(['is_available' => false]);
-            }
+             Reservation::create([
+                'user_id' => $client->id,
+                'segment_id' => $segment->id,
+                'date_reservation' => now(),
+                'statut' => 'Confirmé',
+                'siege_numero' => 14,
+            ]);
         }
     }
 }
