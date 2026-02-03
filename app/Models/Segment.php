6e4fa8410f7a4a9b9a51b4b36f36e3abf5520f88
sahhoutiamine <midoriaduko@gmail.com>
2026-02-03 10:18:59 +0100

    add Segment model

---
+++ app/Models/Segment.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Segment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'programme_id', 
+        'bus_id', 
+        'start_gare_id', 
+        'end_gare_id', 
+        'tarif', 
+        'duree_estimee', 
+        'distance_km'
+    ];
+
+    public function programme()
+    {
+        return $this->belongsTo(Programme::class);
+    }
+
+    public function bus()
+    {
+        return $this->belongsTo(Bus::class);
+    }
+    
+    public function startGare()
+    {
+        return $this->belongsTo(Gare::class, 'start_gare_id');
+    }
+    
+    public function endGare()
+    {
+        return $this->belongsTo(Gare::class, 'end_gare_id');
+    }
+
+    public function calculerPrix()
+    {
+        return $this->tarif;
+    }
+}
