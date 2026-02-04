923378adaa9765f713b12aea2ca89b2b237b3bbc
sahhoutiamine <midoriaduko@gmail.com>
2026-02-04 20:20:26 +0100

    Register booking routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ Route::get('/', function () {
 Route::get('/search', [SearchController::class, 'index'])->name('search.index');
 Route::get('/search/results', [SearchController::class, 'search'])->name('search.results');
 
+use App\Http\Controllers\BookingController;
+Route::get('/booking/create', [BookingController::class, 'create'])->name('booking.create');
+Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
+Route::get('/booking/confirmation/{reservation}', [BookingController::class, 'confirmation'])->name('booking.confirmation');
+
 Route::get('/hello', function () {
     return view('welcome');
 });
