1a48190f5e48958b2c6fa27539b2062787003a05
sahhoutiamine <midoriaduko@gmail.com>
2026-02-04 20:25:17 +0100

    feat: Create new Route Eloquent model with fillable attributes, relationships, and getter methods.

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -25,4 +25,9 @@ class Route extends Model
     {
         return $this->nom . ': ' . $this->description;
     }
+
+    public function getDetailss()
+    {
+        return $this->nom . ': ' . $this->description;
+    }
 }
