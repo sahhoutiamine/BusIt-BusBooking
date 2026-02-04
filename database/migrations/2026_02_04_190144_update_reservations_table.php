95cf633eab1213a89418ddeb2a9afdedb7d7ff39
sahhoutiamine <midoriaduko@gmail.com>
2026-02-04 20:21:52 +0100

    Update reservations table migration

---
+++ database/migrations/2026_02_04_190144_update_reservations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropColumn('siege_numero');
+            $table->decimal('total_price', 10, 2)->default(0);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->integer('siege_numero');
+            $table->dropColumn('total_price');
+        });
+    }
+};
